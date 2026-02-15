6e3aadcfd21cde3b61f8b63330234aac8b66ddcd
LuisLiriano03 <llirianomendez@gmail.com>
2026-02-15 21:07:10 +0100

    added new enums creationOption

---
+++ BackOffice.Domain/Enums/HotelCreationOption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackOffice.Domain.Enums
+{
+    public enum HotelCreationOption
+    {
+        DoNotCreate = 0
+    }
+}
