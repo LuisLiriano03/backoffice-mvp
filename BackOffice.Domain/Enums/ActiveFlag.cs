bcdcb0d4e51c9b20b4af4f9b5af2b3dcdd8b05eb
LuisLiriano03 <llirianomendez@gmail.com>
2026-02-15 19:59:13 +0100

    added enums

---
+++ BackOffice.Domain/Enums/ActiveFlag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackOffice.Domain.Enums
+{
+    public enum ActiveFlag
+    {
+        Inactive = 0,
+        Active = 1
+    }
+}
---
+++ BackOffice.Domain/Enums/DeleteFlag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackOffice.Domain.Enums
+{
+    public enum DeleteFlag
+    {
+        NotDeleted = 0,
+        Deleted = 1
+    }
+}
